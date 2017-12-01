f63a0b7fe580ddde8a9a82850aec7d7ad0b3f395
Jeff <github@314ghz.com>
2017-11-30 19:51:42 -0600

    Initial creation of the project.

---
+++ PowerballWebsite/src/main/java/com/codehobby/powerball/PowerballWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.codehobby.powerball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerballWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerballWebsiteApplication.class, args);
+	}
+}
---
+++ PowerballWebsite/src/test/java/com/codehobby/powerball/PowerballWebsiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codehobby.powerball;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PowerballWebsiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
